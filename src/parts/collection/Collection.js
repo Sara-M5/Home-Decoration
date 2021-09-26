14484e5e59620a3ecafe9271dada03c28de9892b
Sara mahmoodi <74267458+Sara-M5@users.noreply.github.com>
2021-09-26 23:22:50 +0430

    Update Collection.js

--- src/parts/collection/Collection.js
+++ src/parts/collection/Collection.js
@@ -5,13 +5,13 @@ function Collection() {
     return (
         <div className="collection">
           <div className="container ">
-            <div className="row align-items-center">
-            <div className="title col">
+            <div className="row grid-3 align-items-center">
+            <div className="title mt-5">
                 <h2 className="colc-title">Home Decor Collection</h2>
                 <p className="text-muted">Choose what you like for your home</p>
                 <button className="px-4 py-2"> See More</button>
             </div>
-            <div className="m-5 col grid-3">
+            <div className="mt-5">
                 <img className="collection-img " src="/images/living-room(5).jpg"/>
             </div>
            
